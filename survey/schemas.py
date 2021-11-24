556da7e9fd22da390efffdeeeeeacb802e8de9ff
Indosaram <freedom_zero@naver.com>
2021-11-25 01:52:37 +0900

    Added .gitignore, deleted pycache, update validation

--- survey/schemas.py
+++ survey/schemas.py
@@ -1,17 +1,56 @@
-from typing import Optional, List
+from dataclasses import dataclass
+from typing import Optional, List, Union
+
 from pydantic import BaseModel, validator
 
-from survey.question import BaseQuestion
+
+class BaseQuestion:
+    def __init__(
+        self, title: str, required: bool = False, answer: Optional[str] = None
+    ):
+        self.title = title
+        self.required = required
+        self.answer = answer
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, var: dict):
+        if not isinstance(var, dict):
+            raise TypeError("Request body is not JSON")
+
+        instance = cls(**var)
+        for key, value in var.items():
+            if not isinstance(value, type(getattr(instance, key))):
+                raise TypeError(f"Invalid Type: expected {type(value)}")
+        return instance
+
+
+class TextQuestion(BaseQuestion):
+    pass
+
+
+class OptionalQuestion(BaseQuestion):
+    def __init__(
+        self,
+        title: str,
+        options: list,
+        required=False,
+        answer: Optional[str] = None,
+    ):
+        super().__init__(title, required, answer)
+        self.options = options
+
+
+class FileQuesiton(BaseQuestion):
+    pass
 
 
 class Survey(BaseModel):
-    id : Optional[str]
-    title : str
-    data : List[BaseQuestion]
-
-    @validator("data")
-    def validate_data(cls, var) :
-        if not isinstance(var,BaseQuestion):
-            raise TypeError("Invalid Type")
-        return var
-        
+    id: Optional[str]
+    title: str
+    data: List[
+        Union[BaseQuestion, TextQuestion, OptionalQuestion, FileQuesiton]
+    ]
